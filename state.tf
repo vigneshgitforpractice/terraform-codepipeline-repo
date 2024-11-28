3db16dfec0d6554f0b97b16d95c882b5e343930c
vigneshgitforpractice <vigneshrajur.96@gmail.com>
2024-11-28 21:53:12 +0530

    Create state.tf

---
+++ state.tf
@@ -0,0 +1,12 @@
+terraform{
+    backend "s3" {
+        bucket = "mysestestbucket0205"
+        encrypt = true
+        key = "terraform.tfstate"
+        region = "ap-south-1"
+    }
+}
+
+provider "aws" {
+    region = "ap-south-1"
+}
